a91d716d45e85a39e38da207782bf85f37493a57
Erick Simões <erick.simoes@live.com>
2019-05-30 14:29:23 -0300

    Prototype distance recognition

--- JediSlider.ino
+++ JediSlider.ino
@@ -1,12 +1,26 @@
 #include <Ultrasonic.h>
 
 Ultrasonic ultrasonic(12, 13);
+unsigned short distance;
+
+const short range = 5;
+const short distanceBack = 5;
+const short distanceNext = 30;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(ultrasonic.read());
-  delay(1000);
+  distance = ultrasonic.read();
+
+  if(distance >= distanceNext - range && distance <= distanceNext + range) {
+    Serial.println("Next slide");
+  }
+
+  if(distance >= distanceBack - range && distance <= distanceBack + range) {
+    Serial.println("Back slide");
+  }
+  
+  delay(100);
 }
