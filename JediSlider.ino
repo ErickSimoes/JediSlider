d55e6e1934f3aa6769c2010e0751edd9d0a34209
Erick Simões <erick.simoes@live.com>
2019-05-30 14:57:22 -0300

    Add safety button to future implementation of keyboard

--- JediSlider.ino
+++ JediSlider.ino
@@ -3,24 +3,29 @@
 Ultrasonic ultrasonic(12, 13);
 unsigned short distance;
 
+const short enableButton = 2;
+
 const short range = 5;
 const short distanceBack = 5;
 const short distanceNext = 30;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(enableButton, INPUT_PULLUP);
 }
 
 void loop() {
-  distance = ultrasonic.read();
+  if (!digitalRead(enableButton)) {
+    distance = ultrasonic.read();
 
-  if(distance >= distanceNext - range && distance <= distanceNext + range) {
-    Serial.println("Next slide");
+    if (distance >= distanceNext - range && distance <= distanceNext + range) {
+      Serial.println("Next slide");
+    }
+
+    if (distance >= distanceBack - range && distance <= distanceBack + range) {
+      Serial.println("Back slide");
+    }
   }
 
-  if(distance >= distanceBack - range && distance <= distanceBack + range) {
-    Serial.println("Back slide");
-  }
-  
   delay(100);
 }
